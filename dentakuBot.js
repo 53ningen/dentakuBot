48e51afe041a6c28ed9fa648a2dd31d9baf0a4dc
root <root@www5002uo.sakura.ne.jp>
2014-01-25 23:07:31 +0900

    remove BOT_ID setting from source code

--- dentakuBot.js
+++ dentakuBot.js
@@ -1,24 +1,20 @@
 var twitter = require('twitter');
 var bot     = require('./settings.js');
-var BOT_ID  = 'dentaku__Bot';
 
 bot.stream('user', function(stream){
     stream.on('data', function(data){
-        //Streaming APIからデータ受信時のイベント処理
-        if(!('text' in data)) {
-            //console.error("[ERROR] INVARID DATA");
-            return;
-        }
+        if(!('text' in data)) return;
         
         var twitterUserId   = data.user.screen_name;
         var statusId        = data.id_str;
         var isMention       = (data.in_reply_to_user_id !== null);
         var inputExpression = data.text.replace(/[^0-9+\-*/.\(\)]/g, '');
         
-        if(!isMention || twitterUserId === BOT_ID) return;
+        if(!isMention || twitterUserId === bot.BOT_ID) return;
         if(inputExpression == "") return;
 
-        var replyString = '@'+twitterUserId+' '+inputExpression+' = '+eval(inputExpression);
+        var result = eval(inputExpression);
+        var replyString = '@'+twitterUserId+' '+inputExpression+' = '+result;
         var params = {
             in_reply_to_status_id : statusId
         };
@@ -29,4 +25,4 @@ bot.stream('user', function(stream){
     });
 });
 
-console.log('[INFO] dentakuBot.js v0.0.1 READY ');
+console.log('[INFO] dentakuBot.js v0.0.2 READY ');
