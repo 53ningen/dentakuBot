5f7e6877b290314df6468931e8e16fed297925bc
53ningen <root@www5002uo.sakura.ne.jp>
2014-01-26 00:04:31 +0900

    Fixed the problem reacting to tweet in which the BOT is not mentioned. Add Try-Catch around eval()

--- dentakuBot.js
+++ dentakuBot.js
@@ -3,17 +3,28 @@ var bot     = require('./settings.js');
 
 bot.stream('user', function(stream){
     stream.on('data', function(data){
+        //Guard: BOT was mentioned in this tweet?
+        //Guard: Empty tweet?
+        if(!new RegExp('@'+bot.BOT_ID+' ').test(data.text)) return;
         if(!('text' in data)) return;
-        
+
         var twitterUserId   = data.user.screen_name;
         var statusId        = data.id_str;
         var isMention       = (data.in_reply_to_user_id !== null);
         var inputExpression = data.text.replace(/[^0-9+\-*/.\(\)]/g, '');
         
+        
         if(!isMention || twitterUserId === bot.BOT_ID) return;
         if(inputExpression == "") return;
+        
+        var result;
+        try{
+            result = eval(inputExpression);
+        }
+        catch(e){
+            result = "[ERROR] Invalid Expression";
+        }
 
-        var result = eval(inputExpression);
         var replyString = '@'+twitterUserId+' '+inputExpression+' = '+result;
         var params = {
             in_reply_to_status_id : statusId
